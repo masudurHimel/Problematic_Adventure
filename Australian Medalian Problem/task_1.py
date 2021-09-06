7c4ae3f75f08ca5463ec02a831914d900869fe7c
masudurHimel <masudurhimel@gmail.com>
2021-09-06 12:07:14 +0600

    task 1 added

---
+++ Australian Medalian Problem/task_1.py
@@ -0,0 +1,21 @@
+
+# list for storing input data
+
+data = []
+
+while True:
+    name = input("Name: ")
+    medals = input("Medals: ")
+
+    data.append((name,medals))
+
+    input_continue = input("Continue? [y/n] ")
+
+    if input_continue == 'n':
+        break
+
+# sample separator
+print("-------------")
+
+for inputs in data:
+    print(f"{inputs[0]}\t\t{inputs[1]}")
