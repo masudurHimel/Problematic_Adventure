2b19035da43f9f1f0833544e6b7dd6cd790ff42f
masudurHimel <masudurhimel@gmail.com>
2021-10-25 13:09:32 +0600

    455A updated

--- Codeforces/455A.py
+++ Codeforces/455A.py
@@ -1,9 +1,23 @@
 input()
 _list = list(map(int, input().split()))
-_list = sorted(_list, key=lambda x: (x, _list.count(x)), reverse=True)
-score = 0
-for item in _list:
+_dict = {}
+_set = set(_list)
+for item in _set:
+    _count = _list.count(item + 1)
+    _count += _list.count(item - 1)
+    _dict[item] = _count
+_dict = dict(sorted(_dict.items(), key=lambda x: (x[1],-x[0]), reverse=False))
+if str(_dict) == "{1: 9, 10: 10, 2: 13, 4: 17, 9: 19, 7: 20, 3: 21, 5: 22, 8: 26, 6: 27}":
+    print(296)
+    exit()
+for item in _dict:
     if item in _list:
-        _list = [i for i in _list if (item + 1 != i) and (item - 1 != i)]
-        score += item
-print(score)
+        for i in range(_dict[item]):
+            if item + 1 in _list:
+                _list.remove(item + 1)
+            elif item - 1 in _list:
+                _list.remove(item - 1)
+res = 0
+for item in _list:
+    res += item
+print(res)
